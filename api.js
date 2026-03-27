e4f980de69683ca1e74eec814b99d5c98db18be4
DARKSIED009 <darksied009yt@gmail.com>
2026-03-28 04:53:45 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000';
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+});
+
+export const getWorkers = () => api.get('/workers').then(r => r.data);
+export const getWorker = (id) => api.get(`/worker/${id}`).then(r => r.data);
+export const calculateScore = (workerData) => api.post('/score/calculate', workerData).then(r => r.data);
+export const simulateScore = (simRequest) => api.post('/score/simulate', simRequest).then(r => r.data);
+
+export default api;
